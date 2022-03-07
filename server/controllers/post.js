de7438337f06a58d8712905842a6891ab6fe5842
GenHero <genhero@hotmail.com>
2022-03-07 17:07:36 -0500

    created initial controller functions for posts

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,4 +1,46 @@
+import PostMessage from "../models/postMessages.js";
 
-export const getPosts = (req, res) => {
-    res.send('Posts');
+export const getPosts = async (req, res) => {
+    try {
+        const posts = await PostMessage.find();
+
+        res.status(200).json(posts);
+        console.log(posts);
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const createPost = (req, res) => {
+    const body = req.body;
+
+    try {
+
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const updatePost = (req, res) => {
+    const body = req.body;
+
+    try {
+
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
+};
+
+export const deletePost = (req, res) => {
+    const body = req.body;
+
+    try {
+
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message });
+    }
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getPosts } from '../controllers/post.js';
+import { getPosts, createPost, updatePost, deletePost } from '../controllers/post.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send(console.log('Posts'));
-});
+router.get('/', getPosts);
+router.post('/', createPost);
+router.put('/:id', updatePost);
+router.delete('/:id', deletePost);
 
 export default router;
