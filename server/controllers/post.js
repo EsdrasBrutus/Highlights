9a93eb70124e932da0e4b6f282510996b758604c
GenHero <genhero@hotmail.com>
2022-03-07 17:12:19 -0500

    create post function filled out

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -12,7 +12,20 @@ export const getPosts = async (req, res) => {
     }
 };
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
+    const post = req.body;
+
+    const newPost = new PostMessage(post)
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    }
+    catch (err) {
+        res.status(409).json({ message: err.message });
+    }
+};
+
+export const updatePost = async (req, res) => {
     const body = req.body;
 
     try {
@@ -23,18 +36,7 @@ export const createPost = (req, res) => {
     }
 };
 
-export const updatePost = (req, res) => {
-    const body = req.body;
-
-    try {
-
-    }
-    catch (err) {
-        res.status(404).json({ message: err.message });
-    }
-};
-
-export const deletePost = (req, res) => {
+export const deletePost = async (req, res) => {
     const body = req.body;
 
     try {
