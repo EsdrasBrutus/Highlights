b0d0815885f1bfa5bf9d4f28125c53d746d46f6b
GenHero <genhero@hotmail.com>
2022-03-07 16:26:09 -0500

    intial route test

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-const app = express();
-dotenv.config();
 
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+dotenv.config();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+app.use('/posts', postRoutes);
 
 
 const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL)
     .then(() => {
