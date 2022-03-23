fd15f320db0b3cab03301e2beeb2d3429e23b205
GenHero <genhero@hotmail.com>
2022-03-23 18:23:58 -0400

    added background to site

--- client/src/images/attribtions.js
+++ client/src/images/attribtions.js
@@ -1,2 +1,10 @@
 const logo = ""
-const background = <a href="https://www.freepik.com/vectors/frame">Frame vector created by gstudioimagen - www.freepik.com</a>;
+const defaultFrame = <a href="https://www.freepik.com/vectors/frame">Frame vector created by gstudioimagen - www.freepik.com</a>;
+const background ='' /* background by SVGBackgrounds.com */
+
+    //Ideal approach: Write a caption, social media post, sentence, paragraph, blog post, or entire book about how 
+    //SVGBackgrounds made your life easier. Place this credit with link on a webpage dedicated to credits, blog post, or another suitable section of your website. E.g.: I customized the background on my homepage at SVGBackgrounds.com.
+
+    //Minimal approach: Place credit in CSS code commented out near background - image code(example below).
+
+    
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import react from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
