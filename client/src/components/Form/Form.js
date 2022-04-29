02cb6d76d42b6eeecb68cef06e75d24a742b3adc
GenHero <genhero@hotmail.com>
2022-04-29 17:14:32 -0400

    users name now displays on created post/ creator field removed from forms

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,118 +1,151 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { TextField, Button, Typography, Paper } from '@material-ui/core';
-import FileBase from 'react-file-base64';
-import useStyles from './styles';
-import { addPost, updatePost, setCurrentId } from '../../state/actions/postActions';
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import useStyles from "./styles";
+import {
+	addPost,
+	updatePost,
+	setCurrentId,
+} from "../../state/actions/postActions";
+import { useNavigate } from "react-router-dom";
 
 const Form = () => {
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const postId = useSelector(state => state.currentId);
-    const post = useSelector(state => state.currentId ? state.posts.find(post => post._id === state.currentId) : null);
+	const [postData, setPostData] = useState({
+		title: "",
+		message: "",
+		tags: "",
+		selectedFile: "",
+	});
+	const classes = useStyles();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+	const postId = useSelector((state) => state.currentId);
+	const post = useSelector((state) =>
+		state.currentId
+			? state.posts.find((post) => post._id === state.currentId)
+			: null
+	);
+	const user = JSON.parse(localStorage.getItem("profile"));
 
-    const [postData, setPostData] = useState({
-        creator: '',
-        title: '',
-        message: '',
-        tags: '',
-        selectedFile: ''
-    });
+	useEffect(() => {
+		if (post) {
+			setPostData(post);
+		}
+	}, [post]);
 
-    useEffect(() => {
-        if (post) {
-          setPostData(post);
-      }
-    }, [post]);
-    
+	const clear = () => {
+		dispatch(setCurrentId(null));
+		setPostData({
+			title: "",
+			message: "",
+			tags: "",
+			selectedFile: "",
+		});
+	};
 
-    const clear = () => {
-        dispatch(setCurrentId(null));
-        setPostData({
-            creator: '',
-            title: '',
-            message: '',
-            tags: '',
-            selectedFile: ''
-        });
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (postId) {
+			dispatch(updatePost(postId, { ...postData, name: user?.result?.name }));
+		} else {
+			dispatch(addPost({ ...postData, name: user?.result?.name }));
+		}
+  };
+  
+  if (!user?.token) {
+    return (
+      <Paper className={classes.paper}>
+        <Typography variant="h5">Please Sign In to Create a Post</Typography>
+        <Button variant="contained" color="primary" onClick={() => {
+          dispatch(setCurrentId(null));
+          clear();
+          navigate("/auth");
+        }}>
+          Sign In
+        </Button>
+      </Paper>
+    );
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (postId) {
-            dispatch(updatePost(postId, postData));
-        } 
-        else {
-            dispatch(addPost(postData));
-        }
-    }
-
-  return (
-    <Paper className={classes.paper} style={{ padding: '1rem' }}>
-      <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{postId ? 'Editing Post': 'Create a Post'} </Typography>
-        <TextField 
+	return (
+		<Paper className={classes.paper} style={{ padding: "1rem" }}>
+			<form
+				autoComplete="off"
+				noValidate
+				className={`${classes.root} ${classes.form}`}
+				onSubmit={handleSubmit}
+			>
+				<Typography variant="h6">
+					{postId ? "Editing Post" : "Create a Post"}{" "}
+				</Typography>
+				{/* <TextField 
           name='creator' 
           label='Creator' 
           variant='outlined' 
           fullWidth
           value={postData.creator}
           onChange={ (e) => setPostData({ ...postData, creator: e.target.value }) }
-        />
-        <TextField
-          name='title'
-          label='Title'
-          variant='outlined'
-          fullWidth
-          value={postData.title}
-          onChange={ (e) => setPostData({ ...postData, title: e.target.value }) }
-        />
-        <TextField
-          name='message'
-          label='Message'
-          variant='outlined'
-          fullWidth
-          multiline
-          rows={4}
-          value={postData.message}
-          onChange={ (e) => setPostData({ ...postData, message: e.target.value }) }
-        />
-        <TextField 
-          name='tags'
-          label='Hashtags'
-          variant='outlined'
-          fullWidth
-          value={postData.tags}
-          onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') }) }
-        />
-        <div className={classes.fileInput}>
-          <FileBase
-            type='file'
-            multiple={false}
-            onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}
-          />
-        </div>
-        <Button 
-          type='submit'
-          variant='contained'
-          color='primary'
-          className={classes.buttonSubmit}
-        >
-          Submit
-        </Button>
-        <Button 
-          variant='contained'
-          color='secondary'
-          className={classes.buttonSubmit}
-          size='small'
-          onClick={clear}
-        >
-          Clear
-        </Button>
-      </form>
-    </Paper>
-  );
+        /> */}
+				<TextField
+					name="title"
+					label="Title"
+					variant="outlined"
+					fullWidth
+					value={postData.title}
+					onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+				/>
+				<TextField
+					name="message"
+					label="Message"
+					variant="outlined"
+					fullWidth
+					multiline
+					rows={4}
+					value={postData.message}
+					onChange={(e) =>
+						setPostData({ ...postData, message: e.target.value })
+					}
+				/>
+				<TextField
+					name="tags"
+					label="Hashtags"
+					variant="outlined"
+					fullWidth
+					value={postData.tags}
+					onChange={(e) =>
+						setPostData({ ...postData, tags: e.target.value.split(",") })
+					}
+				/>
+				<div className={classes.fileInput}>
+					<FileBase
+						type="file"
+						multiple={false}
+						onDone={({ base64 }) =>
+							setPostData({ ...postData, selectedFile: base64 })
+						}
+					/>
+				</div>
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					className={classes.buttonSubmit}
+				>
+					Submit
+				</Button>
+				<Button
+					variant="contained"
+					color="secondary"
+					className={classes.buttonSubmit}
+					size="small"
+					onClick={clear}
+				>
+					Clear
+				</Button>
+			</form>
+		</Paper>
+	);
 };
 
-export default Form
+export default Form;
