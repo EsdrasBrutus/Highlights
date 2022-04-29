ec4a10b7b709881896fe9d64945eee6886fc107b
GenHero <genhero@hotmail.com>
2022-04-29 16:31:45 -0400

    fixed axios  interceptors for auth verification

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,52 +3,50 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
 API.interceptors.request.use((req) => {
-	if (localStorage.getItem("token")) {
+	if (localStorage.getItem("profile")) {
 		req.headers.Authorization = `Bearer ${
 			JSON.parse(localStorage.getItem("profile")).token
 		}`;
-	}
+    };
 	return req;
 });
 
 export const fetchPosts = async () => {
-    const res = await API.get('/posts');
-    return res.data;
+	const res = await API.get("/posts");
+	return res.data;
 };
 
 export const fetchPost = async (id) => {
-    const res = await API.get(`/posts/${id}`);
-    return res.data;
+	const res = await API.get(`/posts/${id}`);
+	return res.data;
 };
 
-export const createPost = async newPost => {
-    const res = await API.post('/posts', newPost);
-    return res.data;
+export const createPost = async (newPost) => {
+	const res = await API.post("/posts", newPost);
+	return res.data;
 };
 
 export const updatePost = async (id, post) => {
-    const res = await API.patch(`/posts/${id}`, post);
-    return res.data;
+	const res = await API.patch(`/posts/${id}`, post);
+	return res.data;
 };
 
-export const deletePost = async id => {
-    const res = await API.delete(`/posts/${id}`);
-    return res.data;
+export const deletePost = async (id) => {
+	const res = await API.delete(`/posts/${id}`);
+	return res.data;
 };
 
 export const likePost = async (id, post) => {
-    const res = await API.patch(`/posts/${id}/upvote`, post);
-    return res.data;
+	const res = await API.patch(`/posts/${id}/upvote`, post);
+	return res.data;
 };
 
 export const signIn = (formData) => {
-    const res = API.post('/users/signin', formData);
-    return res;
+	const res = API.post("/users/signin", formData);
+	return res;
 };
 
 export const signUp = async (formData) => {
-    const res = await API.post('/users/signup', formData);
-    return res.data;
+	const res = await API.post("/users/signup", formData);
+	return res.data;
 };
-
-
