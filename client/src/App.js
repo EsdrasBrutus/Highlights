edced3e2410e307bd4897652f8cb399da5212b39
GenHero <genhero@hotmail.com>
2022-05-02 18:55:04 -0400

    created PostDetails component and add new routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
 
 const App = () => {
+	const user = JSON.parse(localStorage.getItem("profile"));
+
 	return (
 		<Router>
 			<Container maxWidth="lg">
 				<Navbar />
 				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/auth" element={<Auth />} />
+					<Route path="/" exact element={<Navigate replace to="/posts" />} />
+					<Route path="/posts" element={<Home />} />
+					<Route path="/posts/search" element={<Home />} />
+					<Route path="/posts/:id" element={<PostDetails />} />
+					{user ? (
+						<Route path="/auth" element={<Navigate replace to="/posts" />} />
+					) : (
+						<Route path="/auth" element={<Auth />} />
+					)}
 				</Routes>
 			</Container>
 		</Router>
